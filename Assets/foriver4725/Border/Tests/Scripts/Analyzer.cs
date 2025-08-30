5787ded09686b1d3e7283981bc85642e0d5cbc6b
foriver4725 <hrt67@icloud.com>
2025-08-31 00:00:04 +0900

    Created an analyzer script

---
+++ Assets/foriver4725/Border/Tests/Scripts/Analyzer.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+namespace foriver4725.Border.Tests
+{
+    internal sealed class Analyzer : MonoBehaviour
+    {
+        [SerializeField] private Border border;
+        [SerializeField] private Transform target;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Vector3 pos = target.position;
+                ReadOnlySpan<byte> layers = stackalloc byte[] { 0, 4, 16 };
+
+                // warmup
+                for (int i = 0; i < 8; i++)
+                    _ = border.DoContains(pos, layers);
+                for (int i = 0; i < 8; i++)
+                    _ = border.GetRandomPositionSimply();
+                for (int i = 0; i < 8; i++)
+                    _ = border.GetRandomPositionAccurately();
+
+                Profiler.BeginSample("### Border DoContains ###");
+                {
+                    for (int i = 0; i < 1e6; i++)
+                        _ = border.DoContains(pos, layers);
+                }
+                Profiler.EndSample();
+
+                Profiler.BeginSample("### Border GetRandomPositionSimply ###");
+                {
+                    for (int i = 0; i < 1e6; i++)
+                        _ = border.GetRandomPositionSimply();
+                }
+                Profiler.EndSample();
+
+                Profiler.BeginSample("### Border GetRandomPositionAccurately ###");
+                {
+                    for (int i = 0; i < 1e6; i++)
+                        _ = border.GetRandomPositionAccurately();
+                }
+                Profiler.EndSample();
+            }
+        }
+    }
+}
